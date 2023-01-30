9fcbf6c437b264bc5dce1ba8fe9c1a84dc7c71bf
Karan Desai <karanddata@gmail.com>
2023-01-30 17:07:19 +0000

    SearchInput

---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react"
+import React from "react"
+
+type SearchInputProps = {
+    
+};
+
+const SearchInput:React.FC<SearchInputProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default SearchInput;
